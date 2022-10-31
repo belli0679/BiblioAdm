ce843082be92a0bdd0a20ce1b6151e2aaac4ffb6
belli0679 <93786545+belli0679@users.noreply.github.com>
2022-10-31 13:51:04 -0300

    Aula 74 31/10

--- App.js
+++ App.js
@@ -1,35 +1,13 @@
-import React, {Component} from "react";
-import BottomTabNavigator from "./components/BottomTabNavigator";
-import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
-import * as Font from "expo-font";
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
 
-export default class App extends Component{
-  constructor(){
-    super();
-    this.state = {
-      fontLoaded: false
-    }
-
-  }
-
-  async loadFonts(){
-    await Font.loadAsync({
-      Rajdhani_600SemiBold: Rajdhani_600SemiBold
-    });
-    this.setState({fontLoaded: true})
-
-  }
-
-  componentDidMount(){
-    this.loadFonts();
-  }
-
-  render(){
-    const {fontLoaded} = this.state;
-    if(fontLoaded){
-      return <BottomTabNavigator/>;
-    }
-    return null;
-
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <HomeScreen />
+      </View>
+    );
   }
 }
